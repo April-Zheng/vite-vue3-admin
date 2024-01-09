c33c1b9f13fb797b32810256204597d9e4b088bb
zlp <1131808958@qq.com>
2024-01-09 18:20:31 +0800

    ✨ feat: 构建产物

---
+++ dist/assets/components-GavuHeLh.js
@@ -0,0 +1,1 @@
+import{a9 as t}from"./index-HwAp1Sgz.js";const r=o=>t({url:"/api/components/pro-table",method:"post",data:o}),a=()=>t({url:"/api/components/pro-form",method:"get"});export{a,r as g};
